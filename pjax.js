5fc6dac2b046582ac402bafff3bea8bcd91378d5
bella <bella@madsq.net>
2016-08-10 18:53:34 +0900

    * isSnackkApp flag추가.

--- pjax.js
+++ pjax.js
@@ -1,23 +1,24 @@
-module.exports = function() {
-  return function(req, res, next) {
+module.exports = function () {
+  return function (req, res, next) {
     if (req.header('X-PJAX')) {
-      req.pjax = true;
+      req.pjax = true
     }
 
-    res.renderPjax = function(view, options, fn) {
+    res.renderPjax = function (view, options, fn) {
       if (req.pjax) {
         if (options) {
-          options.layout = false;
+          options.layout = false
         } else {
-          options = {};
-          options.layout = false;
+          options = {}
+          options.layout = false
         }
       }
-      
-      options['isiPhone'] = req.useragent.isiPhone ||req.useragent.isiPad || req.useragent.isiPod;
-      res.partialRender(view, options, fn);
-    };
 
-    next();
-  };
-};
+      options['isiPhone'] = req.useragent.isiPhone || req.useragent.isiPad || req.useragent.isiPod
+      options['isSnackkApp'] = req.useragent.source.indexOf('snackk_app') !== -1
+      res.partialRender(view, options, fn)
+    }
+
+    next()
+  }
+}
